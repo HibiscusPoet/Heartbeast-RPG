f9588b4b97ed8212c05292d0cec5d7b513cffa34
HibiscusPoet <86578158+HibiscusPoet@users.noreply.github.com>
2023-03-23 21:05:56 -0700

    Basic movement animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,11 @@
 extends CharacterBody2D
 
 const SPEED = 100
+const MAX_SPEED = 100
+
+@onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 #	var input_vector = Vector2.ZERO
@@ -8,10 +13,16 @@ func _physics_process(delta):
 #	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = input_vector * SPEED
 	else:
+		#animationPlayer.play("IdleRight")
+		animationState.travel("Idle")
 		velocity = Vector2.ZERO
 	
 	move_and_slide()
